8a6c12c4dfc8d5a0db472a505053b882c19fe0b8
Sunny <77013204+sunnybhakta296@users.noreply.github.com>
2024-07-04 22:46:25 +0530

    Delete src directory

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export const generateRandomId = (length) =>{
-    let result = '';
-    const characters = 'ABCDEFGHI0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
